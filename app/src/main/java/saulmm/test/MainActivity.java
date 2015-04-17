ab17ab109d93d683afdb2e28c8f560bf2db5f28b
Saul M <saulmm2@gmail.com>
2015-04-17 22:16:53 +0200

    Improved the animation

--- app/src/main/java/saulmm/test/MainActivity.java
+++ app/src/main/java/saulmm/test/MainActivity.java
@@ -5,16 +5,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
+import android.widget.FrameLayout;
 
 
 public class MainActivity extends Activity {
 
     private View mFab;
+    private FrameLayout mFabContainer;
 
-    public final static float SCALE_FACTOR = 13f;
-    public final static int FINAL_POSITION_Y = 800;
-    public final static int FINAL_POSITION_X = 400;
-    public final static int ANIMATION_DURATION = 3000;
+    public final static float SCALE_FACTOR      = 13f;
+    public final static int FINAL_POSITION_Y    = 800;
+    public final static int FINAL_POSITION_X    = -400;
+    public final static int ANIMATION_DURATION  = 3000;
+
+    public float mFabSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mFab = findViewById(R.id.fab);
+        mFabSize = getResources().getDimensionPixelSize(R.dimen.fab_size);
+        mFabContainer = (FrameLayout) findViewById(R.id.fab_container);
     }
 
     public void onFabPressed(View view) {
@@ -40,16 +46,20 @@ public class MainActivity extends Activity {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
 
-                if (mFab.getX() > 200) {
+                if (mFab.getY() > 200) {
 
                     if (!flag) {
 
+                        mFabContainer.setY(mFabContainer.getY() + mFabSize);
                         ViewPropertyAnimator scaleAnimation = mFab.animate()
                             .scaleXBy(SCALE_FACTOR)
                             .scaleYBy(SCALE_FACTOR)
                             .setDuration(ANIMATION_DURATION);
 
                         flag = true;
+
+                    } else {
+                        mFab.setY(mFab.getY()- mFabSize);
                     }
                 }
             }
