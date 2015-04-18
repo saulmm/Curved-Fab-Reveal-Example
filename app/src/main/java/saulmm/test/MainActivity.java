6d90a34914e17102cdd34b8ed937e5980fd44597
Saul M <saulmm2@gmail.com>
2015-04-18 11:57:00 +0200

    Added missing media controls

--- app/src/main/java/saulmm/test/MainActivity.java
+++ app/src/main/java/saulmm/test/MainActivity.java
@@ -4,10 +4,8 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.AccelerateInterpolator;
@@ -19,36 +17,15 @@ public class MainActivity extends ActionBarActivity {
 
     private View mFab;
     private FrameLayout mFabContainer;
+    private LinearLayout mControlsContainer;
 
     public final static float SCALE_FACTOR      = 13f;
     public final static int ANIMATION_DURATION  = 600;
     public final static int MINIMUN_X_DISTANCE  = 200;
 
-    public float mFabSize;
-    private LinearLayout mControlsContainer;
-    private boolean flag;
-    private AnimatorListenerAdapter         endRevealListener = new AnimatorListenerAdapter() {
+    private boolean mRevealFlag;
+    private float mFabSize;
 
-        @Override
-        public void onAnimationEnd(Animator animation) {
-
-            super.onAnimationEnd(animation);
-
-            mFab.setVisibility(View.INVISIBLE);
-            mFabContainer.setBackgroundColor(getResources()
-                .getColor(R.color.brand_accent));
-
-            for (int i = 0; i < mControlsContainer.getChildCount(); i++) {
-
-                View v = mControlsContainer.getChildAt(i);
-                ViewPropertyAnimator animator = v.animate()
-                    .scaleX(1).scaleY(1)
-                    .setDuration(ANIMATION_DURATION);
-                animator.setStartDelay(i * 50);
-                animator.start();
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,7 +48,7 @@ public class MainActivity extends ActionBarActivity {
         path.moveTo(0, 0);
         path.curveTo(-200, 200, -400, 100, -600, 50);
 
-        final ObjectAnimator anim = ObjectAnimator.ofObject(this, "buttonLoc",
+        final ObjectAnimator anim = ObjectAnimator.ofObject(this, "fabLoc",
             new PathEvaluator(), path.getPoints().toArray());
 
         anim.setInterpolator(new AccelerateInterpolator());
@@ -80,61 +57,67 @@ public class MainActivity extends ActionBarActivity {
 
         anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
-
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
 
-
                 if (Math.abs(startX - mFab.getX()) > MINIMUN_X_DISTANCE) {
 
-                    if (!flag) {
-
+                    if (!mRevealFlag) {
                         mFabContainer.setY(mFabContainer.getY() + mFabSize / 2);
 
                         mFab.animate()
-                            .setListener(endRevealListener)
                             .scaleXBy(SCALE_FACTOR)
                             .scaleYBy(SCALE_FACTOR)
+                            .setListener(mEndRevealListener)
                             .setDuration(ANIMATION_DURATION);
 
-                        flag = true;
-
+                        mRevealFlag = true;
                     }
                 }
             }
         });
-
-
-
-
     }
 
-    public void restart(View view) {
+    private AnimatorListenerAdapter mEndRevealListener = new AnimatorListenerAdapter() {
 
-        startActivity(new Intent(MainActivity.this, MainActivity.class)
-            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-    }
+        @Override
+        public void onAnimationEnd(Animator animation) {
+
+            super.onAnimationEnd(animation);
+
+            mFab.setVisibility(View.INVISIBLE);
+            mFabContainer.setBackgroundColor(getResources()
+                .getColor(R.color.brand_accent));
+
+            for (int i = 0; i < mControlsContainer.getChildCount(); i++) {
+
+                View v = mControlsContainer.getChildAt(i);
+                ViewPropertyAnimator animator = v.animate()
+                    .scaleX(1).scaleY(1)
+                    .setDuration(ANIMATION_DURATION);
+
+                animator.setStartDelay(i * 50);
+                animator.start();
+            }
+        }
+    };
+
 
     /**
      * We need this setter to translate between the information the animator
      * produces (a new "PathPoint" describing the current animated location)
      * and the information that the button requires (an xy location). The
-     * setter will be called by the ObjectAnimator given the 'buttonLoc'
+     * setter will be called by the ObjectAnimator given the 'fabLoc'
      * property string.
      */
-    public void setButtonLoc(PathPoint newLoc) {
-
-        Log.d("[DEBUG]", "MainActivity setButtonLoc - " +
-            "newLoc.mY " + newLoc.mY);
+    public void setFabLoc(PathPoint newLoc) {
 
         mFab.setTranslationX(newLoc.mX);
 
 
-        if (flag)
+        if (mRevealFlag)
             mFab.setTranslationY(newLoc.mY - (mFabSize / 2));
         else
             mFab.setTranslationY(newLoc.mY);
-
-
     }
 }
